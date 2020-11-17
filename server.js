6ecd69ce19f66c97d8a710a06e75698845b68b53
marawan salman <marawansalman98@gmail.com>
2020-11-18 01:13:27 +0200

    update

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/cars-website'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cars-website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+const Port = process.env.PORT || 8080
 app.listen(process.env.PORT || 8080);
-console.log('App Start');
+console.log('App Start ' + Port);
